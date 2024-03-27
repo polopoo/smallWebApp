2c9d3ec35a6d78cfcfe7a03bbf66d89c8972a701
polina <polopoosha@gmail.com>
2024-03-27 14:56:01 +0300

    file handling is implemented

---
+++ go.mod
@@ -0,0 +1,9 @@
+module tmpHelloWorld
+
+go 1.22
+
+require (
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/mod v0.16.0 // indirect
+	golang.org/x/tools v0.19.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Guestbook struct {
+	SignatureCount int
+	Signatures     []string
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getString("signatures.txt")
+	fmt.Println("%#v\n", signatures)
+	html, err := template.ParseFiles("view.html")
+	check(err)
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+	err = html.Execute(writer, guestbook)
+	check(err)
+}
+
+func NewHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("new.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+func getString(fileName string) []string {
+	var lines []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	check(scanner.Err())
+	return lines
+}
+
+func createHandler(writter http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintln(file, signature)
+	check(err)
+	err = file.Close()
+	check(err)
+	http.Redirect(writter, request, "/guestbook", http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", NewHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
+}
